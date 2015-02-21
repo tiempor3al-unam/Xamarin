b3a9ee3df29ba88feb4146b96a703ff8ec629d81
tiempor3al-unam <jmartinez@docencia.fca.unam.mx>
2015-02-21 09:51:08 -0600

    Cambie Hola mundo por Hello world

--- HolaMundo/HolaMundo/App.cs
+++ HolaMundo/HolaMundo/App.cs
@@ -9,7 +9,7 @@ namespace HolaMundo
 		{	
 			return new ContentPage { 
 				Content = new Label {
-					Text = "Hola mundo!",
+					Text = "Hello world!",
 					VerticalOptions = LayoutOptions.CenterAndExpand,
 					HorizontalOptions = LayoutOptions.CenterAndExpand,
 				},
